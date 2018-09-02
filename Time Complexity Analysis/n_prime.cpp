4bdb49fa9b7066f2ab33d274c2959fff27ddfa14
Tyrion Lannister <Jashan Bhullar>
2018-09-02 22:53:45 +0530

    Two Methods for N being prime or not

---
+++ Time Complexity Analysis/n_prime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void m1_isPrime(int num)
+{
+	for (int i = 2; i < n; ++i)
+	{
+		
+	}
+}
+
+void m2_isPrime(int num)
+{
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
