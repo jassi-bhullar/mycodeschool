8286a86a9621e7c1d86d2012fe31ca2690092afd
Tyrion Lannister <Jashan Bhullar>
2018-09-02 23:12:48 +0530

    Two Methods for N is prime or not

--- Time Complexity Analysis/n_prime.cpp
+++ Time Complexity Analysis/n_prime.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <cmath>
 
-void m1_isPrime(int num)
+bool m1_isPrime(int num)
 {
-	for (int i = 2; i < n; ++i)
+	for(int i = 2; i < num; i++)
 	{
-		
+		if(num%i == 0)
+			return false;
 	}
+	return true;
 }
 
-void m2_isPrime(int num)
+bool m2_isPrime(int num)
 {
-	
+	for(int i = 0; i < sqrt(num); i++)
+	{
+		if(num%i == 0)
+			return false;
+	}
+	return true;
 }
 
 int main(int argc, char const *argv[])
 {
-	
+	int num;
+	std::cin >> num;
+
+	std::cout << "Method 1: Number is " << (m1_isPrime(num) ? "prime" : "not prime") << std::endl;
+	std::cout << "Method 2: Number is " << (m2_isPrime(num) ? "prime" : "not prime") << std::endl;
 	return 0;
 }
